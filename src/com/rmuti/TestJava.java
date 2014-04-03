1622038bb22757527c9268ec2eb81c18b0092b1e
chaluemwut <chaluemwut@hotmail.com>
2014-04-03 15:11:41 +0700

    test

--- src/com/rmuti/TestJava.java
+++ src/com/rmuti/TestJava.java
@@ -3,7 +3,7 @@ package com.rmuti;
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("hello world 123");
+		System.out.println("hello world 123  123");
 	}
 
 }
